f838fd5427a3dd2e1be63901ef73af00a2033082
Najib <najibvlv@gmail.com>
2021-03-11 08:04:39 -0800

    spa 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -42,7 +43,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService,PromotionService],
 
   bootstrap: [AppComponent]
 })
